06ebdc524dd4f489363aa4cec5ba4fc65fa58e3a
Rafael Costrov <rafael.costrov@gmail.com>
2024-04-01 21:46:24 -0300

    Colocando verificação do input, para não ser aceito valores negativos e nem Strings com letras.

--- src/ExericioDois/VerificandoExistencia.java
+++ src/ExericioDois/VerificandoExistencia.java
@@ -8,8 +8,25 @@ public class VerificandoExistencia {
     public static void main(String[] args) {
 
         var leitura = new Scanner(System.in);
-        System.out.println("Digite a quantidade de termos que deseja visualizar na sequência:");
-        long quantidade = leitura.nextInt();
+        long quantidade = 0;
+
+        while (true) {
+            System.out.println("Digite a quantidade de termos que deseja visualizar na sequência:");
+            String input = leitura.nextLine();
+            try {
+                quantidade = Long.parseLong(input);
+                if (quantidade >= 0) {
+                    break;
+                }
+                else {
+                    System.out.println("Não é válido um valor negativo.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, insira um número válido.");
+            }
+        }
+
+
         List listaFibonacci = new ArrayList<>();
         for (int i = 0; i < quantidade; i++) {
             listaFibonacci.add(MontandoSequencia.sequencia(i));
